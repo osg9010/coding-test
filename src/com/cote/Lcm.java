3a1293de79bd4ac549f3922bb223ebea686e3fa9
osg9010 <osg3419010@naver.com>
2024-03-22 15:09:22 +0900

    코딩테스트 문제풀기

---
+++ src/com/cote/Lcm.java
@@ -0,0 +1,38 @@
+package com.cote;
+
+/**
+ *  최소공배수
+ */
+class Solution {
+    public int solution(int[] arr) {
+        int answer = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            answer = lcm(answer, arr[i]);
+        }
+        return answer;
+    }
+
+    public int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
+
+public class Lcm {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        int so = solution.solution(new int[]{2,6,8,14});
+        System.out.println(so);
+
+    }
+}
