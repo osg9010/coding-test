82a5ee3b20b225b925c1f021442f8f69187cbae3
osk <osg3419010@naver.com>
2024-06-03 13:04:18 +0900

    코테연습 - 0단계 평균, 각도기

---
+++ src/com/cote/level0/Protractor.java
@@ -0,0 +1,19 @@
+package com.cote.level0;
+
+public class Protractor {
+    static class Solution {
+        public int solution(int angle) {
+            int answer = 0;
+            if(angle == 180){
+                answer = 4;
+            }else if(angle > 90 && angle < 180){
+                answer = 3;
+            }else if(angle == 90){
+                answer = 2;
+            }else if (angle<90 && angle > 0){
+                answer = 1;
+            }
+            return answer;
+        }
+    }
+}
