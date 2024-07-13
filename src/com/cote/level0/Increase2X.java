0436c8a390bd95c38cfa377ed61cdc8b6b717bde
osg9010 <osg3419010@naver.com>
2024-07-13 10:03:07 +0900

    코테연습 0단계 - 던전 피로도 - 2배만들기

---
+++ src/com/cote/level0/Increase2X.java
@@ -0,0 +1,14 @@
+package com.cote.level0;
+
+public class Increase2X {
+    class Solution {
+        public int[] solution(int[] numbers) {
+            int[] answer = new int[numbers.length];
+
+            for (int i = 0; i < numbers.length; i++) {
+                answer[i] = numbers[i]*2;
+            }
+            return answer;
+        }
+    }
+}
