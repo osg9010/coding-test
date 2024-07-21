5011f3dadfb596a6f7adc2932bba54f5b3f22e5b
osg9010 <osg3419010@naver.com>
2024-07-21 23:25:40 +0900

    코테연습 0단계 - 공배수

---
+++ src/com/cote/level0/CommonMultiple.java
@@ -0,0 +1,13 @@
+package com.cote.level0;
+
+public class CommonMultiple {
+    class Solution {
+        public int solution(int number, int n, int m) {
+            int answer = 0;
+            if(number % n == 0 && number % m == 0) {
+                answer = 1;
+            }
+            return answer;
+        }
+    }
+}
