d62b0886f16b5b23005fe9229d8becbdfe3a04a2
osk <osg3419010@naver.com>
2024-09-19 18:14:34 +0900

    코테 레벨 0 - 제곱수 판별

---
+++ src/com/cote/level0/SquareNumber.java
@@ -0,0 +1,17 @@
+package com.cote.level0;
+
+public class SquareNumber {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+
+            if(Math.sqrt(n) % 1 == 0){
+                answer = 1;
+            }else{
+                answer = 2;
+            }
+
+            return answer;
+        }
+    }
+}
