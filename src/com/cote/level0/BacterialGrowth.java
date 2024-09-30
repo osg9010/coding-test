ca2741723f508578ef01f93223b1f2f09dfc0c37
osk <osg3419010@naver.com>
2024-09-30 14:26:32 +0900

    코테 레벨0 - 박테리아 증식

---
+++ src/com/cote/level0/BacterialGrowth.java
@@ -0,0 +1,14 @@
+package com.cote.level0;
+
+public class BacterialGrowth {
+    class Solution {
+        public int solution(int n, int t) {
+            int answer = n;
+
+            for (int i = 0; i < t; i++) {
+                answer *= 2;
+            }
+            return answer;
+        }
+    }
+}
