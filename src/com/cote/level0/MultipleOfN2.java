f9e0b917a99ced9facdb3aebc11d2c7571af4e38
osk <osg3419010@naver.com>
2024-11-18 16:33:59 +0900

    코테 0 - n의배수

---
+++ src/com/cote/level0/MultipleOfN2.java
@@ -0,0 +1,25 @@
+package com.cote.level0;
+
+public class MultipleOfN2 {
+    static class Solution {
+        public int[] solution(int n, int[] numlist) {
+            int[] answer = {};
+            int count=0;
+            for (int i = 0; i < numlist.length; i++) {
+                if(numlist[i]%n==0){
+                    count++;
+                }
+            }
+            answer = new int[count];
+            count = 0;
+            for (int i = 0; i < numlist.length; i++) {
+                if(numlist[i]%n==0){
+                    answer[count] = numlist[i];
+                    count++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
