67e170c396a18a94fd9c4acd5b13b49f0e31013d
osk <osg3419010@naver.com>
2024-11-19 14:15:46 +0900

    코테 0 - 문자열 붙여서 출력

---
+++ src/com/cote/level0/PrintTogether.java
@@ -0,0 +1,16 @@
+package com.cote.level0;
+
+import java.util.Scanner;
+
+public class PrintTogether {
+
+    public static class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String a = sc.next();
+            String b = sc.next();
+
+            System.out.println(a+b);
+        }
+    }
+}
