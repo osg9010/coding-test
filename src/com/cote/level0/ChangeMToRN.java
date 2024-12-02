bfb28d38bf3b38cf7a08b6fde58bc359d1544284
osk <osg3419010@naver.com>
2024-12-02 13:24:13 +0900

    코테 0 - m을rn으로 치환하기

---
+++ src/com/cote/level0/ChangeMToRN.java
@@ -0,0 +1,10 @@
+package com.cote.level0;
+
+public class ChangeMToRN {
+    class Solution {
+        public String solution(String rny_string) {
+            String answer = rny_string.replaceAll("m", "rn");
+            return answer;
+        }
+    }
+}
