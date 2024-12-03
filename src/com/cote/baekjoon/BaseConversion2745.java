e29f11e67a333909660aa4126a7829cd7ff26256
osk <osg3419010@naver.com>
2024-12-03 14:43:50 +0900

    코테 백준 - 진수 변환 2745

---
+++ src/com/cote/baekjoon/BaseConversion2745.java
@@ -0,0 +1,27 @@
+package com.cote.baekjoon;
+
+import java.util.Scanner;
+
+public class BaseConversion2745 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long answer = 0;
+        String num = in.next();
+        long base = in.nextLong();
+        long numSize = num.length();
+        for (int i = 0; i < num.length(); i++) {
+            numSize = num.length()-1-i;
+            long now = num.charAt(i)-'0';
+            if(now > 9){
+                now -= 7;
+            }
+            while (numSize>0){
+                now *= base;
+                numSize --;
+            }
+            answer += now;
+        }
+
+        System.out.println(answer);
+    }
+}
