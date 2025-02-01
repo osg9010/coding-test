8e09b9f8dab40591ac4415780ae90b9cf65cb498
osg9010 <osg3419010@naver.com>
2025-02-01 11:06:34 +0900

    코테 백준 10926 - ??!

---
+++ src/com/cote/baekjoon/Frighten10926.java
@@ -0,0 +1,11 @@
+package com.cote.baekjoon;
+
+import java.util.Scanner;
+
+public class Frighten10926 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String string = in.nextLine();
+        System.out.println(string+"?!!");
+    }
+}
