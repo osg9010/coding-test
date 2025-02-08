90c503f8315dcb7c596de5996c4b468f13244345
osg9010 <osg3419010@naver.com>
2025-02-08 13:56:47 +0900

    코테 백준 18108 - 불기 연도를 서기 연도로 변환

---
+++ src/com/cote/baekjoon/ADYear18108.java
@@ -0,0 +1,11 @@
+package com.cote.baekjoon;
+
+import java.util.Scanner;
+
+public class ADYear18108 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println(a-543);
+    }
+}
