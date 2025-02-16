1aca692b5d045a8c0371bccca3d1dfbe530875c4
osg9010 <osg3419010@naver.com>
2025-02-16 16:31:27 +0900

    코테 백준 2750 - 수 정렬

---
+++ src/com/cote/baekjoon/NumberSorting2750.java
@@ -0,0 +1,16 @@
+package com.cote.baekjoon;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class NumberSorting2750 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        TreeSet<Integer> integers = new TreeSet<>();
+        int num = in.nextInt();
+        for (int i = 0; i < num; i++) {
+            integers.add(in.nextInt());
+        }
+        integers.iterator().forEachRemaining(System.out::println);
+    }
+}
