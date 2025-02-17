5feb55ac4e58827c45e5862fb2d2d66190a61d89
osk <osg3419010@naver.com>
2025-02-17 11:29:21 +0900

    코테 백준 2587 - 평균과 중앙값

---
+++ src/com/cote/baekjoon/MeanAndMedian2587.java
@@ -0,0 +1,31 @@
+package com.cote.baekjoon;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class MeanAndMedian2587 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        int e = in.nextInt();
+        int sum = a+b+c+d+e;
+        int avg = sum/5;
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(a);
+        integers.add(b);
+        integers.add(c);
+        integers.add(d);
+        integers.add(e);
+
+        Collections.sort(integers);
+
+        System.out.println(avg);
+        System.out.println(integers.get(2));
+
+
+    }
+}
