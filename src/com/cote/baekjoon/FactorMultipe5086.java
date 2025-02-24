efd80a1c54b0b0b79f2df17eb7f5b92f107769bd
osk <osg3419010@naver.com>
2025-02-24 15:15:15 +0900

    코테 백준 5086 - 약수와 배수

---
+++ src/com/cote/baekjoon/FactorMultipe5086.java
@@ -0,0 +1,24 @@
+package com.cote.baekjoon;
+
+import java.util.Scanner;
+
+public class FactorMultipe5086 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (true){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            if(a == 0 && b == 0){
+                break;
+            }else{
+                if(a%b == 0){
+                    System.out.println("multiple");
+                }else if(b % a == 0){
+                    System.out.println("factor");
+                }else{
+                    System.out.println("neither");
+                }
+            }
+        }
+    }
+}
