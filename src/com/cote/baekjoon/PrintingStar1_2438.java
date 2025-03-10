e05192f34c5ad3e46819b1d943c7423e3a204508
osg9010 <osg3419010@naver.com>
2025-03-10 18:35:18 +0900

    코테 백준 2438 - 별찍기1

---
+++ src/com/cote/baekjoon/PrintingStar1_2438.java
@@ -0,0 +1,16 @@
+package com.cote.baekjoon;
+
+import java.util.Scanner;
+
+public class PrintingStar1_2438 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
