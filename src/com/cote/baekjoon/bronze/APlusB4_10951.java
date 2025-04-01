506a45459bc429cf0f1d2dfc8bed04666e59771b
osg9010 <osg3419010@naver.com>
2025-04-01 22:29:49 +0900

    코테 백준 10951 - a+b 4

---
+++ src/com/cote/baekjoon/bronze/APlusB4_10951.java
@@ -0,0 +1,12 @@
+package com.cote.baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class APlusB4_10951 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (true){
+            System.out.println(in.nextInt()+in.nextInt());
+        }
+    }
+}
