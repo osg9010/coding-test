1c9e81a8c7c19af1216bd631c01f341b8860080f
osk <osg3419010@naver.com>
2025-04-15 16:56:19 +0900

    코테 백준 15552 - 문자열 더하기

--- src/com/cote/baekjoon/bronze/APlusB4_15552.java
+++ src/com/cote/baekjoon/bronze/APlusB4_15552.java
@@ -8,10 +8,13 @@ public class APlusB4_15552 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int num = br.read();
+        int num = Integer.parseInt(br.readLine());
         for (int i = 0; i < num; i++) {
-            int intVal = br.read() + br.read();
+            String s = br.readLine();
+            String[] s1 = s.split(" ");
+            int intVal = Integer.parseInt(s1[0])+Integer.parseInt(s1[1]);
             sb.append(intVal+"\n");
         }
+        System.out.println(sb);
     }
 }
