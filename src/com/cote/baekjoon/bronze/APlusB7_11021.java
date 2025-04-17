95a10075561310d3a42d7b78634c97ed3275f05b
osg9010 <osg3419010@naver.com>
2025-04-18 00:03:24 +0900

    코테 백준 11021 - A+B 7

--- src/com/cote/baekjoon/bronze/APlusB7_11021.java
+++ src/com/cote/baekjoon/bronze/APlusB7_11021.java
@@ -7,7 +7,15 @@ import java.io.InputStreamReader;
 public class APlusB7_11021 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
+        for (int i = 0; i < n; i++) {
+            String string = br.readLine();
+            String[] split = string.split(" ");
+            int a = Integer.parseInt(split[0]);
+            int b = Integer.parseInt(split[1]);
+            System.out.println("Case #" + (i + 1) + ": " +( a  + b));
+        }
 
 
     }
