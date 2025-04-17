db1080d9cbcac91b98bd1923086f819f0a482541
osg9010 <osg3419010@naver.com>
2025-04-17 23:59:43 +0900

    코테 백준 11021 - A+B 7

---
+++ src/com/cote/baekjoon/bronze/APlusB7_11021.java
@@ -0,0 +1,14 @@
+package com.cote.baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class APlusB7_11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    }
+}
