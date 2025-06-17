aa19799833d0aebb8283a2656fd764ab1ef72c39
osk <osg3419010@naver.com>
2025-06-17 14:48:11 +0900

    백준 15894 - 수학은 체육과목입니다.

---
+++ src/com/cote/baekjoon/bronze/MathIsAPESubject15894.java
@@ -0,0 +1,11 @@
+package com.cote.baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class MathIsAPESubject15894 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Long a = in.nextLong();
+        System.out.println(a*4);
+    }
+}
